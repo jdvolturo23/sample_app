8d471f3f0bd3398315b1f3f701fc67d8defae8ad
Josh Volturo <jdvolturo@gmail.com>
2015-05-29 23:33:03 -0400

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  get 'users/new/index'
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
