d07b8fc868d956aae7b43d3f2cc24cdb6f77ced7
jdvolturo23 <jdvolturo@gmail.com>
2015-06-02 03:16:16 +0000

    fixed account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
   host = 'http://rails-tutorial-jdvolturo23.c9.io'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'http://rails-tutorial-jdvolturo23.c9.io' }
   
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
